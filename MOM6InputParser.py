75b6a112ab469803049c6e4f4ce6ae6588dffb74
minghangli-uni <minghangl1101@gmail.com>
2024-08-01 18:31:50 +1000

    Fix bugs and provide an example

--- MOM6InputParser.py
+++ MOM6InputParser.py
@@ -2,8 +2,8 @@ import re
 
 class MOM6InputParser(object):
     header_pattern = re.compile(r"^! === (.+) ===")
-    block_list = ['KPP%', '%KPP', 'CVMix_CONVECTION%', '%CVMix_CONVECTION', 'CVMIX_DDIFF%', '%CVMIX_DDIFF']
-    
+    block_list     = ['KPP%', '%KPP', 'CVMix_CONVECTION%', '%CVMix_CONVECTION', 'CVMIX_DDIFF%', '%CVMIX_DDIFF']
+
     def __init__(self):
         self.param_list = {}
         self.commt_list = {}
@@ -11,8 +11,8 @@ class MOM6InputParser(object):
         self.current_var     = None
         self.current_value   = []
         self.current_comment = []
-        self.block_pattern = re.compile(r"|".join(re.escape(i) for i in self.block_list))
-    
+        self.block_pattern = re.compile(r"|".join(re.escape(i) for i in self.block_list))    
+
     def read_lines(self,MOM_input_read_path):
         with open(MOM_input_read_path,'r') as f:
             self.lines = f.readlines()
@@ -33,7 +33,7 @@ class MOM6InputParser(object):
                 self._save_current_param()
                 self._parse_params(line)
             
-            elif block_pattern.search(line): # block_pattern
+            elif self.block_pattern.search(line): # block_pattern
                 self._save_current_param()
                 self._parse_params_block(line)
 
@@ -106,13 +106,9 @@ class MOM6InputParser(object):
                 f.write("\n")
 
 if __name__ == "__main__":
-    #with open('/g/data/tm70/ml0072/MOM6-CICE6/PROFILING/3_2using_scripts/clx-run-o1344i96c96d48-fcpl-s-fm-F-fr-c_no_diags/MOM_input','r') as f:
-    #with open('/Users/u1156405/Downloads/data/scratch/tm70/ml0072/access-om3/archive/my0_1deg_jra55do_ryf_experiment_name/output004/MOM_parameter_doc.all','r') as f:
-        #lines = f.readlines()
-        MOM_input_read_path  = '/Users/u1156405/Downloads/data/scratch/tm70/ml0072/access-om3/archive/my0_1deg_jra55do_ryf_experiment_name/output004/MOM_parameter_doc.all'
-        MOM_input_write_path = '/Users/u1156405/Downloads/data/scratch/tm70/ml0072/access-om3/archive/my0_1deg_jra55do_ryf_experiment_name/output004/MOM_input_new3'
+        MOM_input_read_path  = './MOM_parameter_doc.all'
+        MOM_input_write_path = './MOM_input_new.all'
         parser = MOM6InputParser()
         parser.read_lines(MOM_input_read_path)
         parser.parse_lines()
-        print(parser.param_list)
         parser.writefile_MOM_input(MOM_input_write_path)
